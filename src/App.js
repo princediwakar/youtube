6ffcf517a08aecf3fde3ccc2356df6ca3d24e101
platonian <prince@indshine.com>
2020-07-05 18:25:12 +0530

    Added youtube logo

--- src/App.js
+++ src/App.js
@@ -6,4 +6,3 @@ export const App = () => (
     </div>
 )
 
-export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: false,
+    content: ['./src/**/*.html', './src/**/*.js'],
+  },
   theme: {
     extend: {},
   },
