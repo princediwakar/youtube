f6d0d52414d0ef0cfad11c1807a9ce0e7dcb87bc
Prince Diwakar <princediwakar25@gmail.com>
2026-07-24 09:38:14 +0530

    chore: remove unused legacy JS components

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import { VideoDetails } from './VideoDetails'
-import { Header } from './Header'
-import { SearchBar } from './SearchBar'
-import { VideosList } from './VideosList'
-import { VideoItem } from './VideoItem'
-
-
-
-export { Header, VideoDetails, VideosList, SearchBar, VideoItem }
